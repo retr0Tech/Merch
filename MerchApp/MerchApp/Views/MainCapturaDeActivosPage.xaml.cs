5f34afc671cab82aaa5b4521f5538f839c6651bc
Xavier R <Xaedro2009@hotmail.com>
2019-10-04 19:04:54 -0400

    Code Review

---
+++ MerchApp/MerchApp/Views/MainCapturaDeActivosPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MerchApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MerchApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainCapturaDeActivosPage : TabbedPage
+    {
+        public MainCapturaDeActivosPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainCapturaDeActivosViewModel();
+        }
+    }
+}
