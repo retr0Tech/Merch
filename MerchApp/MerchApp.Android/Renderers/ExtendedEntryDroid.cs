3e5e2f3bb2c2227621fb67e4e0038c7a848036c4
AlfredPolanco <alfredpolanci@gmail.com>
2019-10-09 20:41:59 -0400

    Custom Entry and OrderPage added

---
+++ MerchApp/MerchApp.Android/Renderers/ExtendedEntryDroid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MerchApp.Controls;
+using MerchApp.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ExtendedEntry), typeof(ExtendedEntryDroid))]
+namespace MerchApp.Droid.Renderers
+{
+    public class ExtendedEntryDroid : EntryRenderer
+    {
+        public ExtendedEntryDroid(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(global::Android.Graphics.Color.White);
+            }
+        }
+    }
+}
---
+++ MerchApp/MerchApp.iOS/Renderers/ExtendedEntryiOS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using MerchApp.Controls;
+using MerchApp.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedEntry), typeof(ExtendedEntryiOS))]
+namespace MerchApp.iOS.Renderers
+{
+    public class ExtendedEntryiOS : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.BackgroundColor = UIColor.White;
+                Control.BorderStyle = UIKit.UITextBorderStyle.None;
+            }
+        }
+    }
+}
